6be242681aab85dfe0d0fc9796fc7ffc563f6029
Francisco  Ignacio Sandoval Hevia <francisco.sandoval.hevia@gmail.com>
2024-06-15 17:19:11 -0400

    corrigiendo animacion

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 function llenarJarra() {
-    const cerveza = document.querySelector('.cerveza');
+    const cerveza = document.getElementById('cerveza');
+    const espuma = document.getElementById('espuma');
     const audioLlenado = document.getElementById('audioLlenado');
     
     // Iniciar animación de llenado y reproducir audio
-    cerveza.style.height = '80%'; // 80% para dejar espacio para la espuma
+    cerveza.style.height = '85%'; // 85% para dejar espacio para la espuma
+    espuma.style.height = '15%'; // 15% para la espuma
+    espuma.style.bottom = '85%'; // Ajustar la posición de la espuma
+
     audioLlenado.play();
 
     // Mostrar el mensaje de celebración después de la animación de llenado
